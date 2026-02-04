4f69e0a6a5768f4b326ec1f61da32b44b6419de7
lianyu <1319764245@qq.com>
2026-02-04 22:26:50 +0800

    feat: 新增查询accessKey接口

--- src/main/java/com/yu/apibackend/controller/UserController.java
+++ src/main/java/com/yu/apibackend/controller/UserController.java
@@ -17,6 +17,7 @@ import com.yu.apibackend.model.dto.user.UserRegisterRequest;
 import com.yu.apibackend.model.dto.user.UserUpdateMyRequest;
 import com.yu.apibackend.model.dto.user.UserUpdateRequest;
 import com.yu.apibackend.model.entity.User;
+import com.yu.apibackend.model.vo.AkSkVO;
 import com.yu.apibackend.model.vo.LoginUserVO;
 import com.yu.apibackend.model.vo.UserVO;
 import com.yu.apibackend.service.UserService;
@@ -33,12 +34,7 @@ import me.chanjar.weixin.mp.api.WxMpService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.yu.apibackend.service.impl.UserServiceImpl.SALT;
 
@@ -108,7 +104,7 @@ public class UserController {
      */
     @GetMapping("/login/wx_open")
     public BaseResponse<LoginUserVO> userLoginByWxOpen(HttpServletRequest request, HttpServletResponse response,
-            @RequestParam("code") String code) {
+                                                       @RequestParam("code") String code) {
         WxOAuth2AccessToken accessToken;
         try {
             WxMpService wxService = wxOpenConfig.getWxMpService();
@@ -208,7 +204,7 @@ public class UserController {
     @PostMapping("/update")
     @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
     public BaseResponse<Boolean> updateUser(@RequestBody UserUpdateRequest userUpdateRequest,
-            HttpServletRequest request) {
+                                            HttpServletRequest request) {
         if (userUpdateRequest == null || userUpdateRequest.getId() == null) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
@@ -261,7 +257,7 @@ public class UserController {
     @PostMapping("/list/page")
     @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
     public BaseResponse<Page<User>> listUserByPage(@RequestBody UserQueryRequest userQueryRequest,
-            HttpServletRequest request) {
+                                                   HttpServletRequest request) {
         long current = userQueryRequest.getCurrent();
         long size = userQueryRequest.getPageSize();
         Page<User> userPage = userService.page(new Page<>(current, size),
@@ -278,7 +274,7 @@ public class UserController {
      */
     @PostMapping("/list/page/vo")
     public BaseResponse<Page<UserVO>> listUserVOByPage(@RequestBody UserQueryRequest userQueryRequest,
-            HttpServletRequest request) {
+                                                       HttpServletRequest request) {
         if (userQueryRequest == null) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
@@ -305,7 +301,7 @@ public class UserController {
      */
     @PostMapping("/update/my")
     public BaseResponse<Boolean> updateMyUser(@RequestBody UserUpdateMyRequest userUpdateMyRequest,
-            HttpServletRequest request) {
+                                              HttpServletRequest request) {
         if (userUpdateMyRequest == null) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
@@ -317,4 +313,20 @@ public class UserController {
         ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
         return ResultUtils.success(true);
     }
+
+    /**
+     * 获取用户 ak、sk
+     *
+     * @param request
+     * @return
+     */
+    @GetMapping("/accessKey")
+    public BaseResponse<AkSkVO> getAccessKeyByUserId(HttpServletRequest request) {
+        User loginUser = userService.getLoginUser(request);
+        User user = userService.getById(loginUser.getId());
+        AkSkVO akSkVO = new AkSkVO();
+        BeanUtils.copyProperties(user, akSkVO);
+        return ResultUtils.success(akSkVO);
+    }
+
 }
