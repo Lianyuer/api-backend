ee1b78a5dba4cb5c814613679b0b208f4bb628b5
lianyu <1319764245@qq.com>
2026-02-25 18:53:16 +0800

    feat: Dubbo框架服务提供者demo配置

---
+++ src/main/java/com/yu/apibackend/provider/DemoServiceImpl.java
@@ -0,0 +1,23 @@
+package com.yu.apibackend.provider;
+
+import org.apache.dubbo.config.annotation.DubboService;
+
+import java.util.concurrent.CompletableFuture;
+
+@DubboService
+public class DemoServiceImpl implements DemoService {
+    @Override
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+    @Override
+    public String sayHello2(String name) {
+        return "";
+    }
+
+    @Override
+    public CompletableFuture<String> sayHelloAsync(String name) {
+        return DemoService.super.sayHelloAsync(name);
+    }
+}
